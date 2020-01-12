c98c576acc69be19acd23ed75dd2ddaba22dd681
Mohammed Al-Mohana <m.mohana2022@gmail.com>
2020-01-12 14:41:07 +0300

    test

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
       <h1>This is the first page</h1>
       <h1>This is the first page</h1>
       <h1>This is the first page</h1>
+
+      <h2>This is Mohana</h2>
     </div>
   );
 }
